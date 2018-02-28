747d8cd2a73378246fc0bcc388bcd4d81f13e047
Gabor Koncz <gabee1987@gmail.com>
2018-02-28 11:22:51 +0100

    added create function to vehicle component, binded to form

--- Vega/ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ Vega/ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -45,7 +45,10 @@ export class VehicleFormComponent implements OnInit {
             }
         }
     
-
+        submit() {
+            this.vehicleService.create(this.vehicle)
+                .subscribe(x => console.log(x));
+        }
     
 
 }
